307fc1c60c893cdac44bdb62b62bd9d515562a78
Bettie Jea <bjea@Betties-MacBook-Pro.local>
2018-04-13 00:19:32 -0700

    Corrected negative duration, took care of different dates but w/i inactive_period.

--- src/sessionization.py
+++ src/sessionization.py
@@ -27,63 +27,118 @@ def process_input(in_filename, out_filename, inactive_time):
                 date_curr = row[1]
                 time_curr = row[2]
                 ip = row[0]
-                DATE_FORMAT = '%Y-%M-%D'
+                DATE_FORMAT = '%Y-%m-%d'
                 TIME_FORMAT = '%H:%M:%S'
                 for k, v in dict_log.items():
-                    t_delta = datetime.strptime(time_curr, TIME_FORMAT) - \
-                              datetime.strptime(v[3], TIME_FORMAT)
-                    if t_delta.days == 0:
-                        if t_delta.total_seconds() > inactive_time:
-                            duration = (datetime.strptime(v[3], TIME_FORMAT) -
-                                        datetime.strptime(v[1], TIME_FORMAT)).total_seconds() + 1
+                    # t_delta = datetime.strptime(time_curr, TIME_FORMAT) - \
+                    #           datetime.strptime(v[3], TIME_FORMAT)
+                    # if t_delta.days == 0:
+                    #     if t_delta.total_seconds() > inactive_time:
+                    #         duration = (datetime.strptime(v[3], TIME_FORMAT) -
+                    #                     datetime.strptime(v[1], TIME_FORMAT)).total_seconds() + 1
+                    #         with open(out_filename, mode='a+') as outfile:
+                    #             outfile.write(k + "," + v[0] + " " + v[1] + "," + v[2] + " " + v[3] + ","
+                    #                           + str(int(duration)) + "," + str(dict_count[k]))
+                    #             outfile.write("\n")
+                    #
+                    #         del dict_log[k]
+                    #         del dict_count[k]
+
+                    if date_curr != v[2]:
+                        d_delta = datetime.strptime(date_curr, DATE_FORMAT) - \
+                                  datetime.strptime(v[2], DATE_FORMAT)
+                        t_delta = datetime.strptime(time_curr, TIME_FORMAT) - \
+                                  datetime.strptime(v[3], TIME_FORMAT)
+                        if d_delta.days > 1 or (d_delta.days == 1 and t_delta.days == 0):
+                            duration = datetime.strptime(v[3], TIME_FORMAT) - \
+                                       datetime.strptime(v[1], TIME_FORMAT)
+                            if duration.days < 0:
+                                duration = timedelta(days=0, seconds=t_delta.seconds, microseconds=t_delta.microseconds)
+
+                            duration_secs = duration.total_seconds() + 1
                             with open(out_filename, mode='a+') as outfile:
                                 outfile.write(k + "," + v[0] + " " + v[1] + "," + v[2] + " " + v[3] + ","
-                                              + str(int(duration)) + "," + str(dict_count[k]))
+                                              + str(int(duration_secs)) + "," + str(dict_count[k]))
+                                outfile.write("\n")
+
+                            del dict_log[k]
+                            del dict_count[k]
+                        elif d_delta.days == 1 and t_delta.days < 0:
+                            t_delta = timedelta(days=0, seconds=t_delta.seconds, microseconds=t_delta.microseconds)
+                            if t_delta.total_seconds() > inactive_time:
+                                duration_secs = (datetime.strptime(v[3], TIME_FORMAT) -
+                                            datetime.strptime(v[1], TIME_FORMAT)).total_seconds() + 1
+                                with open(out_filename, mode='a+') as outfile:
+                                    outfile.write(k + "," + v[0] + " " + v[1] + "," + v[2] + " " + v[3] + ","
+                                                  + str(int(duration_secs)) + "," + str(dict_count[k]))
+                                    outfile.write("\n")
+
+                                del dict_log[k]
+                                del dict_count[k]
+
+                    else:
+                        t_delta = datetime.strptime(time_curr, TIME_FORMAT) - \
+                                  datetime.strptime(v[3], TIME_FORMAT)
+                        if t_delta.total_seconds() > inactive_time:
+                            duration = datetime.strptime(v[3], TIME_FORMAT) - \
+                                       datetime.strptime(v[1], TIME_FORMAT)
+                            if duration.days < 0:
+                                duration = timedelta(days=0, seconds=t_delta.seconds, microseconds=t_delta.microseconds)
+
+                            duration_secs = duration.total_seconds() + 1
+                            with open(out_filename, mode='a+') as outfile:
+                                outfile.write(k + "," + v[0] + " " + v[1] + "," + v[2] + " " + v[3] + ","
+                                              + str(int(duration_secs)) + "," + str(dict_count[k]))
                                 outfile.write("\n")
 
                             del dict_log[k]
                             del dict_count[k]
 
-                    if date_curr != v[2]:
-                        d_delta = datetime.strptime(date_curr, DATE_FORMAT) - \
-                                  datetime.strptime(v[2], DATE_FORMAT)
-                        if d_delta.days == 1:
-                            t_delta = timedelta
-
-                    else:
-                        # t_delta = datetime.strptime(time_curr, TIME_FORMAT) - \
-                        #           datetime.strptime(v[3], TIME_FORMAT)
-                        # if t_delta.total_seconds() > inactive_time:
-                        #     duration = (datetime.strptime(v[3], TIME_FORMAT) - \
-                        #       datetime.strptime(v[1], TIME_FORMAT)).total_seconds() + 1
-                        #     with open(out_filename, mode='a+') as outfile:
-                        #         outfile.write(k + "," + v[0] + " " + v[1] + "," + v[2] + " " + v[3] + ","
-                        #                       + str(int(duration)) + "," + str(dict_count[k]))
-                        #         outfile.write("\n")
-                        #
-                        #     del dict_log[k]
-                        #     del dict_count[k]
-
                 if ip in dict_log:
-                    li_prev_entry = [ip] + dict_log.get(row[0])
+                    li_prev_entry = dict_log.get(row[0])
 
-                    if date_curr == li_prev_entry[3]:
+                    if date_curr == li_prev_entry[2]:
                         t_delta = datetime.strptime(time_curr, TIME_FORMAT) - \
-                                  datetime.strptime(li_prev_entry[4], TIME_FORMAT)
+                                  datetime.strptime(li_prev_entry[3], TIME_FORMAT)
                         if t_delta.total_seconds() < inactive_time + 1:
                             dict_count[ip] += 1
-                            dict_log.update({ip: [li_prev_entry[1], li_prev_entry[2], date_curr, time_curr]})
+                            dict_log.update({ip: [li_prev_entry[0], li_prev_entry[1], date_curr, time_curr]})
+                    else:
+                        d_delta = datetime.strptime(date_curr, DATE_FORMAT) - \
+                                  datetime.strptime(li_prev_entry[2], DATE_FORMAT)
+                        t_delta = datetime.strptime(time_curr, TIME_FORMAT) - \
+                                  datetime.strptime(li_prev_entry[3], TIME_FORMAT)
+                        if d_delta.days == 1 and t_delta.days < 0:
+                            t_delta = timedelta(days=0, seconds=t_delta.seconds, microseconds=t_delta.microseconds)
+                            if t_delta.total_seconds() < inactive_time + 1:
+                                dict_count[ip] += 1
+                                dict_log.update({ip: [li_prev_entry[0], li_prev_entry[1], date_curr, time_curr]})
+
+                            #     duration = (datetime.strptime(v[3], TIME_FORMAT) -
+                            #                 datetime.strptime(v[1], TIME_FORMAT)).total_seconds() + 1
+                            #     with open(out_filename, mode='a+') as outfile:
+                            #         outfile.write(k + "," + v[0] + " " + v[1] + "," + v[2] + " " + v[3] + ","
+                            #                       + str(int(duration)) + "," + str(dict_count[k]))
+                            #         outfile.write("\n")
+                            #
+                            #     del dict_log[k]
+                            #     del dict_count[k]
+                            # else:
 
                 else:
                     dict_log.update({ip: [row[1], row[2], date_curr, time_curr]})
                     dict_count[ip] = 1
 
     for k, v in dict_log.items():
-        duration = (datetime.strptime(v[3], TIME_FORMAT) - \
-                    datetime.strptime(v[1], TIME_FORMAT)).total_seconds() + 1
+        duration = datetime.strptime(v[3], TIME_FORMAT) - \
+                   datetime.strptime(v[1], TIME_FORMAT)
+        if duration.days < 0:
+            duration = timedelta(days=0, seconds=t_delta.seconds, microseconds=t_delta.microseconds)
+
+        duration_secs = duration.total_seconds() + 1
         with open(out_filename, mode='a+') as outfile:
             outfile.write(k + "," + v[0] + " " + v[1] + "," + v[2] + " " + v[3] + ","
-                          + str(int(duration)) + "," + str(dict_count[k]))
+                          + str(int(duration_secs)) + "," + str(dict_count[k]))
             outfile.write("\n")
 
         del dict_log[k]
